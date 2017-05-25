ab0da2687e8d81ab7c24ad6035bb9f9c3cb9e3cc
junlyyouny <junlyyouny@gmail.com>
2017-05-25 17:32:40 +0800

    使用友言评论

--- resources/views/blog/partials/page-footer.blade.php
+++ resources/views/blog/partials/page-footer.blade.php
@@ -2,7 +2,7 @@
 <hr>
 <div class="container">
   <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-    @include('blog.partials.duoshuo')
+    @include('blog.partials.uyan')
   </div>
 </div>
 @endif
